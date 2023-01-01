15586fee381604a2d13581434295bdd777caaff8
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-01-01 19:29:55 +0530

    Added react scripts

---
+++ 03-Dynamic-style-with-css-module/src/components/UI/Button/Button.js
@@ -0,0 +1,44 @@
+import React from 'react';
+// import styled from 'styled-components';
+import styles from './Button.module.css'; 
+//aove all classes will be added in tags as properties
+
+
+//attacked template literal
+/*
+const Button = styled.button`
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+
+    @media(min-width: 768px){
+      width: auto;
+    }
+
+    // for pseudo selector use &
+    &:focus {
+      outline: none;
+    }
+
+    &:hover,
+    &:active {
+      background: #ac0e77;
+      border-color: #ac0e77;
+      box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+    }
+`;
+*/
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
